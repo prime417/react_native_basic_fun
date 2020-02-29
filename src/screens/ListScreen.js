b30a5f0917739d22e02861a50d955016723ca79b
Pankaj Salunkhe <pankajds417@gmail.com>
2020-03-01 00:41:02 +0530

    Add ListScreen component

---
+++ src/screens/ListScreen.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+
+const ListScreen = () => {
+  const friends = [
+    { name: "Friend #1" },
+    { name: "Friend #2" },
+    { name: "Friend #3" },
+    { name: "Friend #4" },
+    { name: "Friend #5" },
+    { name: "Friend #6" }
+  ];
+
+  return (
+    <FlatList
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      keyExtractor={friend => friend.name}
+      data={friends}
+      renderItem={({ item }) => {
+        return <Text style={styles.textStyle}>{item.name}</Text>;
+      }}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  textStyle: {
+    marginVertical: 50,
+    marginHorizontal: 10
+  }
+});
+
+export default ListScreen;
