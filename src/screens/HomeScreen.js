9987cc3ca0365189339a886864744f2bd5a20f03
Pankaj Salunkhe <pankajds417@gmail.com>
2020-03-08 18:02:15 +0530

    Add reusable imageDetail component

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -14,6 +14,10 @@ const HomeScreen = ({ navigation }) => {
         title="Go to List Demo"
         onPress={() => navigation.navigate("List")}
       />
+      <Button
+        title="Go to Image screen"
+        onPress={() => navigation.navigate("Image")}
+      />
     </View>
   );
 };
