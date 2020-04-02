3ba4fd85d863c53ceb630062121a230b25c41811
Pankaj Salunkhe <pankajds417@gmail.com>
2020-04-02 09:11:31 +0530

    Add Box Screen with AlignItem property

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import ColorScreen from "./src/screens/ColorScreen";
 import SquareScreen from "./src/screens/SquareScreen";
 import CounterScreenReduce from "./src/screens/CounterScreenReduce";
 import TextScreen from "./src/screens/TextScreen";
+import BoxScreen from "./src/screens/BoxScreen";
 
 const navigator = createStackNavigator(
   {
@@ -20,7 +21,8 @@ const navigator = createStackNavigator(
     Color: ColorScreen,
     Square: SquareScreen,
     CounterReduce: CounterScreenReduce,
-    TextDemo: TextScreen
+    TextDemo: TextScreen,
+    Box: BoxScreen
   },
   {
     initialRouteName: "Home",
